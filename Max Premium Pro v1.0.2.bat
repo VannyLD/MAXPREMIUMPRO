ba8f8b985c2a39410b3956c4f804d023be29a529
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-04-08 00:34:59 +0700

    Update Max Premium Pro v1.0.2.bat

--- Max Premium Pro v1.0.2.bat
+++ Max Premium Pro v1.0.2.bat
@@ -1,13 +1,21 @@
 @echo off
+title Max Premium Pro - Updater
+
 echo Closing current application...
-taskkill /IM "MAX_PREMIUM_PRO_v1.0.1.exe" /F
-timeout /t 2 /nobreak
+taskkill /IM "Max Premium Pro.exe" /F >nul 2>&1
+timeout /t 2 /nobreak >nul
 
 echo Downloading new version...
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MAXPREMIUMPRO/releases/download/v1.0.1/MAX.PREMIUM.PRO.v1.0.2.exe', 'MAX_PREMIUM_PRO_v1.0.2.exe')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MAXPREMIUMPRO/releases/download/v1.0.2/Max%20Premium%20Pro%20v1.0.2.exe', 'Max Premium Pro.exe')"
 
-echo Starting new version...
-start MAX_PREMIUM_PRO_v1.0.2.exe
+if exist "Max Premium Pro.exe" (
+    echo Starting new version...
+    start "" "Max Premium Pro.exe"
+) else (
+    echo ERROR: Failed to download the new version!
+    pause
+    exit /b
+)
 
-echo Cleaning up...
+echo Cleaning up updater script...
 del "%~f0"
