3dd843f4c133311979217d19b806c7a3129893db
Vanny <147380142+VannyLD@users.noreply.github.com>
2025-04-07 21:19:44 +0700

    Update MAX PREMIUM PRO v1.0.2.bat
    G1

--- MAX PREMIUM PRO v1.0.2.bat
+++ MAX PREMIUM PRO v1.0.2.bat
@@ -1,22 +1,13 @@
 @echo off
-setlocal
-
 echo Closing current application...
-taskkill /IM "MAX PREMIUM PRO v1.0.1.exe" /F >nul 2>&1
-timeout /t 2 /nobreak >nul
+taskkill /IM "MAX_PREMIUM_PRO_v1.0.1.exe" /F
+timeout /t 2 /nobreak
 
 echo Downloading new version...
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MAXPREMIUMPRO/releases/download/v1.0.1/MAX.PREMIUM.PRO.v1.0.2.exe', 'MAX PREMIUM PRO v1.0.2.exe')"
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/VannyLD/MAXPREMIUMPRO/releases/download/v1.0.1/MAX.PREMIUM.PRO.v1.0.2.exe', 'MAX_PREMIUM_PRO_v1.0.2.exe')"
 
-if exist "MAX PREMIUM PRO v1.0.2.exe" (
-    echo Starting new version...
-    start "" "MAX PREMIUM PRO v1.0.2.exe"
+echo Starting new version...
+start MAX_PREMIUM_PRO_v1.0.2.exe
 
-    echo Cleaning up...
-    del "%~f0"
-) else (
-    echo Failed to download the new version. Please check your internet connection or try again later.
-    pause
-)
-
-endlocal
+echo Cleaning up...
+del "%~f0"
